73b38d4197c34a81875b0a3e1cd75e57c1bc9529
Monofon <monofon@tramberend.de>
2025-10-08 06:12:00 +0200

    update

--- src/cgg/Image.java
+++ src/cgg/Image.java
@@ -6,48 +6,42 @@ import tools.*;
 public class Image implements tools.Image {
 
     // ---8<--- missing-implementation
-    // TODO Provides storage for the image data. For each pixel in the image
-    // three double values are needed to store the pixel components.
+    // Provides storage for the image data.
     public Image(int width, int height) {
     }
 
-    // TODO Stores the RGB color components for one particular pixel addressed
+    // Stores the RGB color components for one pixel addressed
     // by it's coordinates in the image.
     public void setPixel(int x, int y, Color color) {
     }
 
-    // TODO Retrieves the RGB color components for one particular pixel addressed
+    // Stores the image data in a PNG file.
+    public void writePng(String name) {
+        System.out.format("Implement function `cgg.Image.writePng` to actually write image `%s`\n", name);
+        // ImageWriter.writePng(name, data, width, height);
+    }
+    // --->8---
+
+    // Retrieves the RGB color components for one particular pixel addressed
     // by it's coordinates in the image.
     public Color getPixel(int x, int y) {
         return Color.black;
     }
-    // --->8---
-
-    public void writePng(String name) {
-        System.out.format("Implement function `cgg.Image.writePng` to actually write image `%s`\n", name);
-        // TODO This call also needs to be adjusted once Image() and setPixel()
-        // are implemented. Use
-        // ImageWriter.writePng(String name, double[] data, int width, int height) to
-        // write the image data to disk in PNG format.
-    }
 
     public void writeHdr(String name) {
         System.out.format("Implement function `cgg.Image.writeHdr` to actually write image `%s`\n", name);
-        // TODO This call also needs to be adjusted once Image() and setPixel()
-        // are implemented. Use
-        // ImageWriter.writePng(String name, double[] data, int width, int height) to
-        // write the image data to disk in OpenEXR format.
+        // ImageWriter.writeHdr(name, data, width, height);
     }
 
     public int width() {
-        // TODO This is just a dummy value to make the compiler happy. This
+        // This is just a dummy value to make the compiler happy. This
         // needs to be adjusted such that the actual width of the Image is
         // returned.
         return 0;
     }
 
     public int height() {
-        // TODO This is just a dummy value to make the compiler happy. This
+        // This is just a dummy value to make the compiler happy. This
         // needs to be adjusted such that the actual height of the Image is
         // returned.
         return 0;
